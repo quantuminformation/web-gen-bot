86fb831268f6566ebcc3fb728f77c7a063cdcfe6
Nikos Katsikanis <nkatsikanis@gmail.com>
2019-06-06 12:11:15 +0300

    initial attempt to load a site

--- src/api.ts
+++ src/api.ts
@@ -6,11 +6,36 @@ type SiteDefinition = {
 };
 
 export class Api {
+  sites: SiteDefinition[] = null;
   loadSites() {
     axios
       .get("./src/resources/news_sites.json")
       .then((response: AxiosResponse<SiteDefinition[]>) => {
-        console.log(response.data[0]);
+        this.sites = response.data;
+
+        axios
+          .get(this.sites[0].url)
+          .then(response => {
+            if (response.status === 200) {
+              console.log(response.data);
+            }
+          })
+          .catch(err => {
+            throw new Error(err);
+          });
+
+        /*        this.sites.forEach(site => {
+          axios
+            .get(site.url)
+            .then(response => {
+              if (response.status === 200) {
+                console.log(response.data);
+              }
+            })
+            .catch(err => {
+              throw new Error(err);
+            });
+        })*/
       })
       .catch(err => {});
   }
