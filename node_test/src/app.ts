65b538afdce8a1f105c4cfa5c154dcd3505600cc
Nikos Katsikanis <nkatsikanis@gmail.com>
2019-06-07 11:58:28 +0300

    fix node types and as ts-node npx

--- node_test/src/app.ts
+++ node_test/src/app.ts
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import { Request, Response } from "express";
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req: Request, res: Response) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
